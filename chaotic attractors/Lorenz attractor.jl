9ba67650caef5a554d57270d349f89dab37e0b52
Gayathri <126940938+GayathriK0@users.noreply.github.com>
2023-06-02 16:57:30 +0530

    Update Lorenz attractor.jl

--- chaotic attractors/Lorenz attractor.jl
+++ chaotic attractors/Lorenz attractor.jl
@@ -1,1 +1,31 @@
+using DynamicalSystems, Plots, DifferentialEquations
 
+begin
+	plotly()
+	GC
+end
+
+function ls!(du,u,p,t)
+  x,y,z=u
+  σ,ρ,β=p
+  du[1] = σ*(y-x)
+  du[2] = (x*(ρ-z))-y
+  du[3]=(x*y)-(β*z)
+  return
+end
+
+begin
+	state=[1.5,0.6,1.0]
+	σ=10
+    ρ=28
+	β=8/3
+	p=[σ,ρ,β]
+	lsl = ContinuousDynamicalSystem(ls!,state,p)
+	tr = trajectory(lsl, 2000.0, Ttr = 1000)
+end
+#Time series
+plot(tr[:1][:,1])
+#Phase portrait
+plot(tr[:1][:,1],tr[:1][:,2])
+#3D
+plot(tr[:1][:,1],tr[:1][:,2],tr[:1][:,3])
